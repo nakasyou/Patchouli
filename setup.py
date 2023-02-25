700ce8ecf035dd39bcab8a5fd47f583cd8368bfc
nakasyou <nakasyou1103@gmail.com>
2023-02-25 18:51:05 +0900

    change

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md",encoding="utf-8") as f:
+    long_description=f.read();
+
+setup(
+    name="Patchouli",
+    version="0.1.0",
+    install_requires=[],
+    description="Patchouli is LINE app's history text parser.",
+    long_description=long_description,
+    license="MIT",
+    keywords='line patchouliv history parser',
+    author="nakasyou",
+    url="https://github.com/nakasyou/Patchouli"
+)
